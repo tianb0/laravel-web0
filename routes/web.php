a720cea3606e0ea0fe4e387b01c1aea3a8e76a61
Tianbo <tianbo@hey.com>
2023-02-22 22:50:51 -0800

    layout, partial, 404

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,38 @@ use App\Models\Listing;
 |
 */
 
-Route::get('/', function() {
+Route::get('/', function () {
   return view('listings', [
     'heading' => 'Latest Listings',
     'listings' => Listing::all(),
   ]);
 });
 
-Route::get('/listings/{id}', function($id){
-    return view('listing', [
-      'listing' => Listing::find($id)
-    ]);
-  });
+/* Route::get('/listings/{id}', function($id){ */
+/*     return view('listing', [ */
+/*       'listing' => Listing::find($id) */
+/*     ]); */
+/*   }); */
 
-Route::get('/check', function() {
+/* Route::get('/listings/{id}', function ($id) { */
+/*   $listing = Listing::find($id); */
+
+/*   if ($listing) { */
+/*     return view('listing', [ */
+/*       'listing' => $listing */
+/*     ]); */
+/*   } else { */
+/*     abort('404'); */
+/*   } */
+/* }); */
+
+Route::get('/listings/{listing}', function (Listing $listing) {
+  return view('listing', [
+    'listing' => $listing
+  ]);
+});
+
+Route::get('/check', function () {
   return 'good';
 });
 
