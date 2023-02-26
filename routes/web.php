4d39e1a80ce08763da40130e60a6b37dbb7cb46f
Tianbo <tianbo@hey.com>
2023-02-26 10:22:32 -0800

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -39,6 +40,16 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // should below /create
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+Route::get('/register', [UserController::class, 'create']);
+
+Route::post('/users', [UserController::class, 'store']);
+
+Route::post('/logout', [UserController::class, 'logout']);
+
+Route::get('/login', [UserController::class, 'login']);
+
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
 
 /* Route::get('/', function () { */
 /*   return view('listings', [ */
